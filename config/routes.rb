aff0e8ac88cec58d32aeafb2b43cbefc78e29769
Dineth Rajapakse <dineth.r1@outlook.com>
2023-04-27 12:44:06 +1000

    i feel okay about this now - to whomever is following

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,17 @@
 Rails.application.routes.draw do
+  
   root :to => 'pages#home'
-  # get 'photos/index'
-  # get 'photos/new'
-  # get 'photos/edit'
-  # get 'photos/show'
-  # get 'journals/index' => 'journals#index'
-  # get 'journals/new'=> 'journals#new'
-  # post 'journals/new' => 'journals#create'
-  # get 'journals/edit'=> ''
-  # get 'journals/show'
+
 
   resources :journals 
   resources :photos
   
   resources :users, :only => [:new, :index, :create]
   get '/signup', to: 'users#new'
+
+  get '/signin' => 'session#new'
+  post '/signin' => 'session#create'
+  delete 'signin' => 'session#destroy'
   
 
 end
